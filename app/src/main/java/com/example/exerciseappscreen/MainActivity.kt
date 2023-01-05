26d9bfe07a0d4ac1c68c9663037a6c9dcc2717f9
Pavlo LaptopAcer <pavlo.gorduz@gmail.com>
2023-01-05 22:58:55 +0200

    added buttons

--- app/src/main/java/com/example/exerciseappscreen/MainActivity.kt
+++ app/src/main/java/com/example/exerciseappscreen/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.exerciseappscreen
 
-import android.graphics.Color.RED
-import android.hardware.camera2.params.RggbChannelVector.RED
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +14,10 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,15 +41,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+var bigText = "big text"
+
+//var smallText = "small text"
+var picture = R.drawable.yellow_tire2
+
+
 @Composable
 fun ArtScreen() {
 
     Column(
-        modifier = Modifier.padding(0.dp)
+        modifier = Modifier.padding(0.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
 
         ) {
         Image(
-            painter = painterResource(id = R.drawable.yellow_tire2),
+            painter = painterResource(id = picture),
             contentDescription = null,
             modifier = Modifier
                 .rotate(0.0f)
@@ -63,8 +72,53 @@ fun ArtScreen() {
             Modifier.height(0.dp)
         )
 
-        Texting(text = "Gddd")
+        Texting(text = bigText)
 
+        Text(
+            buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Gray)) {
+                    append("small text bold ")
+                }
+                withStyle(style = SpanStyle(color = Color.LightGray)) {
+                    append("(and normal)")
+                }
+            },
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(bottom = 40.dp)
+        )
+
+        Row() {
+            Button(
+                colors = buttonColors(Color.Red),
+                onClick = { bigText = "1st button clicked" },
+                enabled = true,
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .width(110.dp)
+            ) {
+                Text(
+                    text = "Previous",
+                    color = Color.Yellow
+                )
+            }
+
+            Button(
+                colors = buttonColors(Color.Green),
+                onClick = { bigText = "2nd button pressed" },
+                enabled = true,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .width(110.dp)
+            ) {
+                Text(
+                    text = "Next",
+                    color = Color.Yellow
+                )
+            }
+        }
     }
 }
 
